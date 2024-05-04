5895e490a66a5ae23af01d10593775b6f04ff80b
rostyslav.bornitskyi <doofixxx@gmail.com>
2024-05-04 13:25:37 +0300

    add examples of bad input

--- fp/src/app.ts
+++ fp/src/app.ts
@@ -18,7 +18,19 @@ console.log(
 const f = R.compose(div, R.map(strToNumber), split(','))
 
 console.log(
-  f('1,2'),
-  f('2,10'),
-  f('3,15'),
+  '1,2', f('1,2'), '\n',
+  '2,10', f('2,10'), '\n',
+  '3,15', f('3,15'), '\n',
 );
+
+console.log();
+console.log('example of bad input: ');
+console.log();
+
+console.log(
+  '0,2', f('0,2'), '\n',
+  '2', f('2'), '\n',
+  'not_a_number', f('not_a_number'), '\n',
+);
+
+
