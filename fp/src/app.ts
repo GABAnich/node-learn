9062d08cede91219e8e15079b1974f3bb71fd179
rostyslav.bornitskyi <doofixxx@gmail.com>
2024-05-04 22:43:07 +0300

    add declarative version

--- fp/src/app.ts
+++ fp/src/app.ts
@@ -31,9 +31,25 @@ const imperative: F = (user) => {
   };
 };
 
+const isUserValid = (user: User): boolean => !user.isDeleted;
+const addEmail = (user: User) => ({ user, email: user.email });
+type AddText = (res: { user: User, email: string }) => { user: User, email: string, text: string };
+const addText: AddText = (res) => ({
+  ...res,
+  text: `Hello, ${res.user.name.toUpperCase()}. The video ${res.user?.lastViewedVideo?.title} has ${res.user?.lastViewedVideo?.likesNumber} likes`,
+});
+type RemoveUser = (res: { user: User, email: string, text: string }) => { email: string; text: string };
+const removeUser: RemoveUser = (res) => ({ email: res.email, text: res.text });
 const declarative: F = (user) => pipe(
   user,
-  () => ({ errMessage: 'todo' }),
+  O.fromPredicate(isUserValid),
+  O.map(addEmail),
+  O.map(addText),
+  O.map(removeUser),
+  O.matchW(
+    () => ({ errMessage: 'something went wrong' }),
+    (res) => res,
+  ),
 );
 
 console.log(imperative(validUser), declarative(validUser));
