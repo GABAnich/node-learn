f6054e5922063e4859e63115c019d710bf51e732
rostyslav.bornitskyi <doofixxx@gmail.com>
2024-05-04 12:46:23 +0300

    remake div to accept array

--- fp/src/app.ts
+++ fp/src/app.ts
@@ -1,17 +1,22 @@
+import R from 'ramda';
+
 type Split = (delim: string) => (str: string) => string[]
 const split: Split = (delim) => (str) => str.split(delim);
 
 type StrToNumber = (str: string) => number;
 const strToNumber: StrToNumber = (str) => parseFloat(str);
 
-type Div = (a: number) => (b: number) => number;
-const div: Div = (a) => (b) => b / a;
+type Div = ([a, b]: [number, number]) => number;
+const div: Div = ([a, b]) => b / a;
 
 console.log(
   split(',')('1,2'),
   strToNumber('12'),
-  div(2)(10),
+  div([2, 10]),
 );
 
+/*
 console.log(
+  R.compose(div, strToNumber, split)('1,2')
 );
+*/
