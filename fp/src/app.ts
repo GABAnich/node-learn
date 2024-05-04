f1a957778aef3d01f65d1c56e33ea173a95b8fee
rostyslav.bornitskyi <doofixxx@gmail.com>
2024-05-04 12:41:38 +0300

    add split strToNumber div functions

--- fp/src/app.ts
+++ fp/src/app.ts
@@ -1,1 +1,17 @@
-console.log('hi')
+type Split = (delim: string) => (str: string) => string[]
+const split: Split = (delim) => (str) => str.split(delim);
+
+type StrToNumber = (str: string) => number;
+const strToNumber: StrToNumber = (str) => parseFloat(str);
+
+type Div = (a: number) => (b: number) => number;
+const div: Div = (a) => (b) => b / a;
+
+console.log(
+  split(',')('1,2'),
+  strToNumber('12'),
+  div(2)(10),
+);
+
+console.log(
+);
